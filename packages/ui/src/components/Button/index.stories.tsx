f5ac9a7ba7b3674ea8c17eaf2ed5a83536178e2e
rasel-stacklearner <rasel@stacklearner.com>
2023-12-14 16:28:52 +0600

    Refactor Button component styles and add new variants and sizes

--- packages/ui/src/components/Button/index.stories.tsx
+++ packages/ui/src/components/Button/index.stories.tsx
@@ -1,10 +1,17 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
-import Button from ".";
+import { Button } from ".";
 
 const meta: Meta<typeof Button> = {
   title: "Components/Button",
   component: Button,
+  argTypes: {
+    children: {
+      table: {
+        disable: true,
+      },
+    },
+  },
   parameters: {
     layout: "centered",
   },
@@ -12,11 +19,48 @@ const meta: Meta<typeof Button> = {
 
 export default meta;
 
-type story = StoryObj<typeof meta>;
+type Story = StoryObj<typeof meta>;
 
-export const Solid: story = {
+export const Solid: Story = {
   args: {
+    children: "Solid",
     variant: "solid",
-    children: "Solid Button",
+  },
+};
+
+export const Outline: Story = {
+  args: {
+    children: "Outline",
+    variant: "outline",
+  },
+};
+
+export const Ghost: Story = {
+  args: {
+    children: "Ghost",
+    variant: "ghost",
+  },
+};
+
+// Button with different Sizes
+
+export const Small: Story = {
+  args: {
+    children: "Small",
+    size: "sm",
+  },
+};
+
+export const Medium: Story = {
+  args: {
+    children: "Medium",
+    size: "md",
+  },
+};
+
+export const Large: Story = {
+  args: {
+    children: "Large",
+    size: "lg",
   },
 };
--- packages/ui/src/components/Button/index.tsx
+++ packages/ui/src/components/Button/index.tsx
@@ -1,15 +1,70 @@
+import { cn } from "@/utils";
+import { cva, VariantProps } from "class-variance-authority";
 import { ComponentProps } from "react";
+/* The `cva` function is used to create a variant object that defines different styles for a component
+based on different variants. In this case, it is used to define the styles for a button component. 
+*/
 
-type ButtonProps = ComponentProps<"button"> & {
-  variant?: "solid" | "outline" | "ghost";
+const buttonStyles = cva(
+  [
+    "w-full",
+    "rounded-md",
+    "font-semibold",
+    "focus:outline-none",
+    "disabled:cursor-not-allowed",
+  ],
+  {
+    variants: {
+      variant: {
+        solid: "",
+        outline: "border-2",
+        ghost: "transition-colors duration-300",
+      },
+      size: {
+        sm: "px-4 py-2 text-sm",
+        md: "px-4 py-2 text-base",
+        lg: "px-6 py-3 text-lg",
+      },
+      colorScheme: {
+        primary: "text-white",
+      },
+    },
+    compoundVariants: [
+      {
+        variant: "solid",
+        colorScheme: "primary",
+        className: "bg-primary-500 hover:bg-primary-600",
+      },
+      {
+        variant: "outline",
+        colorScheme: "primary",
+        className:
+          "text-primary-600 border-primary-500 bg-transparent hover:bg-primary-100",
+      },
+      {
+        variant: "ghost",
+        colorScheme: "primary",
+        className: "text-primary-600 bg-transparent hover:bg-primary-100",
+      },
+    ],
+    defaultVariants: {
+      variant: "solid",
+      size: "md",
+      colorScheme: "primary",
+    },
+  }
+);
+
+type ButtonProps = ComponentProps<"button"> & VariantProps<typeof buttonStyles>;
+
+export const Button = ({
+  variant,
+  size,
+  colorScheme,
+  className,
+  ...props
+}: ButtonProps) => {
+  const classes = cn(buttonStyles({ variant, size, colorScheme, className }));
+  // classes output : w-full rounded-md font-semibold focus:outline-none disabled:cursor-not-allowed px-4 py-2 text-base text-white bg-primary-500 hover:bg-primary-600
+  return <button {...props} type="button" className={classes} />;
 };
-
-const Button = ({ ...props }: ButtonProps) => {
-  return (
-    <button {...props} type="button" className="text-blue-500">
-      {props.children}
-    </button>
-  );
-};
-
-export default Button;
