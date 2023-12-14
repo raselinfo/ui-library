5b5e8a7b5718e47c3cef40c54949be9ff6186e0f
rasel-stacklearner <rasel@stacklearner.com>
2023-12-14 12:45:32 +0600

    Add format and storybook scripts to package.json, and create cn function in utils

--- packages/ui/src/utils/index.ts
+++ packages/ui/src/utils/index.ts
@@ -1,1 +1,13 @@
-import {} from "tailwind-merge";
+import clsx, { ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+/**
+ * The function `cn` is a TypeScript function that merges multiple class values into a single string.
+ * @param {ClassValue[]} classes - The `classes` parameter is a rest parameter that allows you to pass
+ * in multiple class values. Each class value can be a string or an array of strings.
+ * @returns {string} - The `cn` function returns a single string that contains all of the class values
+ * example input: cn("class1", ["class2", "class3"])
+ * example output: "class1 class2 class3"
+ */
+
+export const cn = (...classes: ClassValue[]) => twMerge(clsx(classes));
